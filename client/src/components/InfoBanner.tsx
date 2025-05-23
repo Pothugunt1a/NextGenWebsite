50ee15b2dbdb37de482c258a8711243de851ab38
Pothugunt1a <pranithapothuguntla04@gmail.com>
2025-05-23 12:17:26 -0500

    6th Commit

--- client/src/components/InfoBanner.tsx
+++ client/src/components/InfoBanner.tsx
@@ -15,11 +15,9 @@ export default function InfoBanner() {
           <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-8">
             <div className="flex-1">
               <h2 className="text-1xl font-bold text-slate-900 mb-3">
-                For Further Information And To Learn More About Our AI Solutions <br />Along With How We Can Support Your Organisation In Harnessing The Full Potential Of AI Make Contact With Our Experienced IT Staff.
+                For Further Information And To Learn More About Our AI Solutions <br />Along With How We Can Support Your Organisation In Harnessing <br />The Full Potential Of AI Make Contact With Our Experienced IT Staff.
               </h2>
             
-                
-             
             </div>
             <div className="flex flex-col items-start md:items-end gap-4">
               <div className="text-sm font-medium text-primary uppercase tracking-wide">REACH OUT NOW!</div>
