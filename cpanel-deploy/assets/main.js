1f2b334e8ec44fad7ebb01b56c5bde3e7367aa08
pranithapcareer <42981636-pranithapcareer@users.noreply.replit.com>
2025-06-10 08:42:34 +0000

    Restored to 'e1263e4095e4a3cb288df8280cca9997054f4086'
    Replit-Restored-To: e1263e4095e4a3cb288df8280cca9997054f4086

--- cpanel-deploy/assets/main.js
+++
@@ -1,7 +0,0 @@
-// RT NextGenAI Production Bundle - Optimized for cPanel deployment
-// This is a placeholder for the production build
-// The actual build will be generated when you run: npm run build
-console.log("RT NextGenAI - Loading application...");
-
-// For immediate deployment, you'll need to run the production build process
-// which will generate the optimized JavaScript and CSS files
